d9ab1484f6a2bbc0b7cbf4ecbd8c3e678091b11b
timyshark <61923560+timyshark@users.noreply.github.com>
2020-12-06 17:51:45 -0800

    Add jenkins file

---
+++ src/app/students/studentsList.model.ts
@@ -0,0 +1,39 @@
+[
+    {
+        "id": 1,
+        "first_name": "Leda",
+        "last_name": "Mayer",
+        "email": "schumm.vivien@example.org",
+        "isActive": 0,
+        "gender": "M",
+        "school_code": "NONE",
+        "school_name": "School is not defined",
+        "dob": "2005-04-21",
+        "picturePath": "/assets/images/male1.png"
+    },
+    {
+        "id": 2,
+        "first_name": "Bradly",
+        "last_name": "Schneider",
+        "email": "lehner.benton@example.com",
+        "isActive": 0,
+        "gender": "M",
+        "school_code": "NONE",
+        "school_name": "School is not defined",
+        "dob": "1971-05-26",
+        "picturePath": "/assets/images/male1.png"
+    },
+    {
+        "id": 3,
+        "first_name": "Keyshawn",
+        "last_name": "Homenick",
+        "email": "krista.friesen@example.com",
+        "isActive": 0,
+        "gender": "M",
+        "school_code": "NONE",
+        "school_name": "School is not defined",
+        "dob": "2009-04-06",
+        "picturePath": "/assets/images/male1.png"
+    }
+]
+ 
